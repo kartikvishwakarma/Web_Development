596dab75072d8c808d4f92a39773b5c4b2b6af94
kartik <kartik@kartik-Predator-G3-572>
2020-02-17 22:51:15 +0530

    set app.js port and Ip to generic

--- app.js
+++ app.js
@@ -67,6 +67,10 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, function(){
-	console.log("Server started at PORT: 3000");
+// app.listen(3000, function(){
+// 	console.log("Server started at PORT: 3000");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started at " + process.env.PORT + " with Ip: " + process.env.IP);
 });
